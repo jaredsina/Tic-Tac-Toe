b05d3896698d8efa1716ead9b96346cd5524b7e3
Sina Hernandez <herjaredsinai@gmail.com>
2022-08-17 11:19:32 -0400

    Check if user picked valid position

--- javascript.js
+++ javascript.js
@@ -4,11 +4,20 @@ const Gameboard = (()=>{
     const displayBoard= ()=>{console.log(board)}
     const changeBoard=(position, mark)=>{
         board.splice(position, 1,mark);
+        displayBoard()
+    }
+    const checkPosition=(position,mark)=>{
+        if(board[position]==""){
+            changeBoard(position,mark)
+        }else{
+            console.log("Spot Taken");
+        }
     }
     const deleteBoard=()=>{
         board = ["","","","","","","","",""]
+        displayBoard()
     };
-    return{changeBoard, deleteBoard,displayBoard,getBoard}
+    return{changeBoard, deleteBoard,displayBoard,getBoard,checkPosition}
 })();
 
 const Player = (name,mark) =>{
@@ -17,18 +26,17 @@ const Player = (name,mark) =>{
 };
 
 const game = (()=>{
-    let gameOver=false;
+    const playerOne = Player(prompt("Player 1 Name: "),"x");
+    const playerTwo = Player(prompt("Player 2 Name: "),"o");
+    let gameOver=true;
     while(gameOver!=true){
-        Gameboard.displayBoard();
-        
+
     }
+    Gameboard.checkPosition(3,playerOne.mark);
+    Gameboard.checkPosition(3,playerTwo.mark);
+
     return{};
 })();
-const playerOne = Player(prompt("Player 1 Name: "),"x");
-const playerTwo = Player(prompt("Player 2 Name: "),"o");
-Gameboard.changeBoard(3,playerOne.mark);
-Gameboard.displayBoard();
-Gameboard.deleteBoard();
-Gameboard.displayBoard();
+
 
 
